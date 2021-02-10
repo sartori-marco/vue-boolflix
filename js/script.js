0f556f09b6f598a8f923403d663fb23af3cb1a2c
marco sartori <marcosartori98@yahoo.com>
2021-02-10 15:59:16 +0100

    inizio metodo ricerca

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 var app = new Vue({
   el: '#root',
   data:{
-
+    lang: 'it-IT',
+    api: "ed7ae3f6b9b2bb241e47691c175aafdd",
+    query: '',
+    films: ''
   },
 
   methods:{
+    // METODO RICERCA DA INPUT
+    ricerca(){
+      axios
+      .get("https://api.themoviedb.org/3/search/movie",{
+        params: {
+          language: this.lang,
+          api_key: this.api,
+          query: this.query
+        }
+      })
+      .then((result) => {
+        this.films = result.data.results;
+      })
+    }
 
   }
 });
