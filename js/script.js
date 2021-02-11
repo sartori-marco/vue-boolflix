7cf1daf2996b53b36a29246f79550c84de124b50
marco sartori <marcosartori98@yahoo.com>
2021-02-11 10:12:07 +0100

    aggiunta bandiere

--- js/script.js
+++ js/script.js
@@ -1,25 +1,45 @@
 var app = new Vue({
   el: '#root',
   data:{
+    valoreInput: '',
     lang: 'it-IT',
     api: "ed7ae3f6b9b2bb241e47691c175aafdd",
     query: '',
-    films: ''
+    films: '',
+    series: '',
+    // immagine: 'https://image.tmdb.org/t/p/w342'
   },
 
   methods:{
     // METODO RICERCA DA INPUT
     ricerca(){
+      // FILM
       axios
       .get("https://api.themoviedb.org/3/search/movie",{
         params: {
           language: this.lang,
           api_key: this.api,
-          query: this.query
+          query: this.valoreInput
         }
       })
       .then((result) => {
         this.films = result.data.results;
+        this.valoreInput = '';
+
+      })
+
+      // SERIE
+      axios
+      .get("https://api.themoviedb.org/3/search/tv",{
+        params: {
+          language: this.lang,
+          api_key: this.api,
+          query: this.valoreInput
+        }
+      })
+      .then((result) => {
+        this.series = result.data.results;
+        this.valoreInput = '';
       })
     }
 
